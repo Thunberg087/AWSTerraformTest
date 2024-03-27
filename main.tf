39493a3f1f898ad500089d340029ca307d23613f
Jonathan Thunberg <thunberg00@gmail.com>
2024-03-27 15:17:22 +0100

    Init Terraform AWS

---
+++ main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "eu-north-1"
+}
+
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0d74f1e79c38f2933"
+  instance_type = "t3.micro"
+  
+
+  tags = {
+    Name = "Main"
+  }
+}
