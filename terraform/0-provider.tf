e980105a5f9e1f76d0f486de01a2f2c8eef5733e
Forsrobin <forsrobin@gmail.com>
2024-03-28 13:45:13 +0100

    Add VPC and provider configuration files, remove unnecessary files, and add Go module dependencies

---
+++ terraform/0-provider.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "eu-north-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+  
+  required_version = ">= 0.12"
+}
