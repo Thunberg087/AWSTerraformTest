aa7e50888b5a65c5134753979c26e6e0f36f7933
Forsrobin <forsrobin@gmail.com>
2024-03-28 15:12:18 +0100

    Add AWS provider configuration and variables

--- terraform/0-provider.tf
+++ terraform/0-provider.tf
@@ -1,5 +1,17 @@
+terraform {
+  cloud {
+    organization = "aviliax" 
+    workspaces {
+      name = "test-project"
+    }
+  }
+}
+
 provider "aws" {
   region = "eu-north-1"
+
+  access_key= var.access_key
+  secret_key= var.secret_key
 }
 
 terraform {
---
+++ terraform/variable.tf
@@ -0,0 +1,11 @@
+variable "access_key" {
+  type        = string
+  default     = ""
+  description = "access_key"
+}
+
+variable "secret_key" {
+  type        = string
+  default     = ""
+  description = "secret_key"
+}
